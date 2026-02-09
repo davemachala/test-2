4a39c81b683fbbf66fa83f0ab94bcfd98d58eca2
davemachala <83541210+davemachala@users.noreply.github.com>
2026-02-09 18:45:48 +0100

    Update trip and destination station IDs

--- app.py
+++ app.py
@@ -8,15 +8,15 @@ from datetime import datetime, timedelta
 app = Flask(__name__)
 
 # --- NASTAVENÍ ---
-TRIP_ID_LIKE = "-CZTRAINT-SC-507" 
-CILOVA_STANICE_ID = "-SR70ST-343624" # Olomouc hl.n.
+TRIP_ID_LIKE = "-CZTRAINT-EC-223" 
+CILOVA_STANICE_ID = "-SR70ST-333120" # Červenka
 START_STANICE_NAZEV = "Praha hl.n."  # Výchozí stanice pro 0 % na progress baru
 SOUBOR_DATA = "data.csv"
 DOBA_JIZDY_Z_UNICOVA = 30 # minut
 
 # Globální proměnné
 STANICE_DB = {} # { "343624": {"nazev": "Olomouc", "lat":..., "lon":...} }
-NAZEV_CILE = "Olomouc hl.n." 
+NAZEV_CILE = "Červenka" 
 START_STANICE_ID = None # Najdeme automaticky podle názvu
 
 def nacti_stanice_z_csv():
